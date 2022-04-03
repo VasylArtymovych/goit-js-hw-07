970a444b6b706f59fbbf5290a48dc5fb1575c6dd
VasylArtymovych <art777vasss@gmail.com>
2022-04-03 19:21:49 +0300

    update task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRefs = document.querySelector('.gallery');
+
+galleryRefs.addEventListener('click', onGalleryImgClick);
+
+function onGalleryImgClick(evt) {
+    evt.preventDefault();
+    
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    const modalImg = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${modalImg}" width="800" height="600">
+`, { closable: true });
+    
+    instance.show(() => window.addEventListener('keydown', onModalPressEsc));
+
+    function onModalPressEsc(evt) {
+    const isEsc = evt.code === 'Escape';
+    
+    if (isEsc) {
+        instance.close(() => window.removeEventListener('keydown', onModalPressEsc));
+        }
+    };
+};
+
+function createGalleryMarkup(obj) {
+    const murkup = obj.map(createGalleryElement).join('');
+    
+    galleryRefs.innerHTML = murkup;
+};
+createGalleryMarkup(galleryItems);
+
+function createGalleryElement({preview, original, description}) { 
+    const element = `<a class="gallery__link" href='${original}'><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>`;
+    return element;
+};
