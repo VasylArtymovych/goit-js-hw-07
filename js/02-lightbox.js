39eff40835c6dfac4e5acb3954dbd475d1750cfa
VasylArtymovych <art777vasss@gmail.com>
2022-04-05 22:58:18 +0300

    update task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,20 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryRef = document.querySelector('.gallery');
 
-
-
-galleryRef.addEventListener('click', onImageClick)
-
-function onImageClick(evt) {
-    evt.preventDefault();
-    if (evt.target.nodeName !== "IMG") return;
-
-    let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
-    gallery.open();
-    
-}
-
-
 function addGalleryItemsImgs(obj) {
     const gallery = obj.map(createGalleryElement).join('');
 
@@ -27,3 +13,9 @@ addGalleryItemsImgs(galleryItems);
 function createGalleryElement({preview, original, description}) {
     return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`
 };
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+gallery.on(() => {
+    gallery.next();
+});
