24f036e151e2a8a4f24faae06be64ba63e0751c6
VasylArtymovych <art777vasss@gmail.com>
2022-05-07 22:14:33 +0300

    added arrows-right-left-moove

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,14 @@
 import { galleryItems } from './gallery-items.js';
-
+let currentIndex = 0;
 const galleryRef = document.querySelector('.gallery');
+
+const instance = basicLightbox.create(`
+    <img src="" width="800" height="600">`, 
+    {
+        closable: true,
+        onShow: (instance) => { window.addEventListener('keydown', onModalPressEsc) },
+        onClose: (instance) => {window.removeEventListener('keydown', onModalPressEsc)},
+    });
 
 galleryRef.addEventListener('click', onGalleryImgClick);
 
@@ -11,34 +19,54 @@ function onGalleryImgClick(evt) {
         return;
     };
 
-    const modalImgToShow = evt.target.dataset.source;
+    let imgToShow = evt.target.dataset.source;
+    currentIndex = Number( evt.target.dataset.action);
 
-    const instance = basicLightbox.create(`
-    <img src="${modalImgToShow}" width="800" height="600">`, 
-    {
-        closable: true,
-        onShow: (instance) => { window.addEventListener('keydown', onModalPressEsc) },
-        onClose: (instance) => {window.removeEventListener('keydown', onModalPressEsc)},
-    });
-    
+    setUrl(imgToShow);
     instance.show();
 };
 
 function onModalPressEsc(evt) {
-    const isEsc = evt.code === 'Escape';
     
-    if (isEsc) {
-        instance.close();
-        }
-    };
+    switch (evt.code){
+        case 'Escape': 
+            instance.close();
+        break;
+        case 'ArrowRight':
+            currentIndex += 1
+            if(currentIndex > galleryItems.length - 1){
+                currentIndex = 0;
+            }
+            setUrl(galleryItems[currentIndex].original);
+        break;
+        case 'ArrowLeft': 
+            currentIndex -=1;
+            if (currentIndex < 0){
+                currentIndex = galleryItems.length - 1;
+            }
+            setUrl(galleryItems[currentIndex].original);
+        break;
+        default: console.log('press Right or Left');
+    }
+};
+
+function setUrl(url){
+    instance.element().querySelector('img').src = url;
+};    
 
 function createGalleryMarkup(obj) {
     const murkup = obj.map(createGalleryElement).join('');
-    
     galleryRef.innerHTML = murkup;
 };
 createGalleryMarkup(galleryItems);
 
-function createGalleryElement({preview, original, description}) { 
-    return element = `<a class="gallery__link" href='${original}'><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>`; 
+function createGalleryElement({preview, original, description}, index) { 
+    return `<a class="gallery__link" href='${original}'>
+    <img 
+    class="gallery__image" 
+    src="${preview}" 
+    data-source="${original}" 
+    data-action="${index}"
+    alt="${description}">
+    </a>`; 
 };
